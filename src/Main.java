febb6c94f42424875392bf05eabb0479b03c1246
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-23 11:57:57 +0900

    박기준

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println("12");
         System.out.println("abc");
         System.out.println("박기준입니다.");
-        
         System.out.println("branch");
     }
 }
